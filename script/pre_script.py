a4e7d34d181802d3a80a2d40ef3106514b4970f0
bkleiner <b.kleiner@str8labs.com>
2022-07-16 18:52:40 +0200

    pio: add optimization flags

--- script/pre_script.py
+++ script/pre_script.py
@@ -1,6 +1,7 @@
 Import("env")
 
-SDCC_OPTS = ["--model-large", "--opt-code-speed"]
+SDCC_OPTS = ["--model-large", "--opt-code-speed", "--peep-return",
+             "--fomit-frame-pointer", "--allow-unsafe-read"]
 
 env.Append(
     CFLAGS=SDCC_OPTS,
