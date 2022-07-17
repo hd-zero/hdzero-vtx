034d2fe61bcaee02e611cc392781433c4b70bccf
bkleiner <b.kleiner@str8labs.com>
2022-07-17 12:52:40 +0200

    spi: add delay after asserting DO

--- src/spi.h
+++ src/spi.h
@@ -1,9 +1,10 @@
 #ifndef __SPI_H_
 #define __SPI_H_
 
-void SPI_Write(uint8_t trans, uint16_t addr, uint32_t dat_h, uint32_t dat_l);
-void SPI_Read (uint8_t trans, uint16_t addr, uint32_t* dat_h, uint32_t* dat_l);
-void SPI_Init();
+#include "stdint.h"
 
+void SPI_Write(uint8_t trans, uint16_t addr, uint32_t dat_h, uint32_t dat_l);
+void SPI_Read(uint8_t trans, uint16_t addr, uint32_t *dat_h, uint32_t *dat_l);
+void SPI_Init();
 
 #endif
