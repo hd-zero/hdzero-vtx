3a932ca9305c094c19321fd7622d3d57affacbfa
bkleiner <b.kleiner@str8labs.com>
2022-07-30 16:07:51 +0200

    print: workaround compiler warning

--- src/print.h
+++ src/print.h
@@ -7,11 +7,20 @@ void _debugf(const char *fmt, ...);
 void _verbosef(const char *fmt, ...);
 
 #ifdef _DEBUG_MODE
-#define debugf _debugf
-#define verbosef _verbosef
+#define debugf(args...) _debugf(args)
+#define verbosef(args...) _verbosef(args)
 #else
+
+#ifdef SDCC
 #define debugf 
 #define verbosef
+#else
+// workaround keils warning C275: expression with possibly no effect
+static void _nopf(const char *fmt, ...) { fmt = fmt; }
+#define debugf _nopf
+#define verbosef  _nopf
+#endif
+
 #endif
 
 #endif
