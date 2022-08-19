995d1f10e9b10b0339cf4863fc0a78f5742fc16b
bkleiner <b.kleiner@str8labs.com>
2022-08-19 14:29:14 +0200

    remove varadic macro for keil compability

--- src/print.h
+++ src/print.h
@@ -1,5 +1,5 @@
-#ifndef __PRINT_H__
-#define __PRINT_H__
+#ifndef __PRINT_H_
+#define __PRINT_H_
 
 #include <stdarg.h>
 
@@ -7,20 +7,20 @@ void _debugf(const char *fmt, ...);
 void _verbosef(const char *fmt, ...);
 
 #ifdef _DEBUG_MODE
-#define debugf(args...) _debugf(args)
-#define verbosef(args...) _verbosef(args)
+#define debugf _debugf
+#define verbosef _verbosef
 #else
 
 #ifdef SDCC
-#define debugf 
+#define debugf
 #define verbosef
 #else
 // workaround keils warning C275: expression with possibly no effect
 static void _nopf(const char *fmt, ...) { fmt = fmt; }
 #define debugf _nopf
-#define verbosef  _nopf
+#define verbosef _nopf
 #endif
 
 #endif
 
-#endif
+#endif /* __PRINT_H_ */
