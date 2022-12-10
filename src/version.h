d1cf694af2d379b1628ce26c544465c82df25bcc
ligenxxxx <59721724+ligenxxxx@users.noreply.github.com>
2022-12-10 15:41:12 +0800

    Update version.h

--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
 #ifndef __VERSION_H_
 #define __VERSION_H_
 
-#define VTX_VERSION_MAJOR       0 // increment when a major release is made (big new feature, etc)
-#define VTX_VERSION_MINOR       1 // increment when a minor release is made (small new feature, change etc)
+#define VTX_VERSION_MAJOR       1 // increment when a major release is made (big new feature, etc)
+#define VTX_VERSION_MINOR       0 // increment when a minor release is made (small new feature, change etc)
 #define VTX_VERSION_PATCH_LEVEL 0 // increment when a bug is fixed
 #define VTX_VERSION_STRING      STR(VTX_VERSION_MAJOR) "." STR(VTX_VERSION_MINOR) "." STR(VTX_VERSION_PATCH_LEVEL)
 
