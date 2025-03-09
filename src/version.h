e374e0a133c7709e1a23a77445fe582788617e51
Geoff Sim <geoffsim59@gmail.com>
2025-03-09 14:18:27 +0000

    Include CAM switch version information (temporary).

--- src/version.h
+++ src/version.h
@@ -4,7 +4,7 @@
 #define VTX_VERSION_MAJOR       1 // increment when a major release is made (big new feature, etc)
 #define VTX_VERSION_MINOR       7 // increment when a minor release is made (small new feature, change etc)
 #define VTX_VERSION_PATCH_LEVEL 0 // increment when a bug is fixed
-#define VTX_VERSION_STRING      STR(VTX_VERSION_MAJOR) "." STR(VTX_VERSION_MINOR) "." STR(VTX_VERSION_PATCH_LEVEL)
+#define VTX_VERSION_STRING      STR(VTX_VERSION_MAJOR) "." STR(VTX_VERSION_MINOR) "." STR(VTX_VERSION_PATCH_LEVEL) " CAM 3.1"
 
 #define _STR(x) #x
 #define STR(x)  _STR(x)
